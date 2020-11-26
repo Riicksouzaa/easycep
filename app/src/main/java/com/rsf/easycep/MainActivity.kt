fbf847778349563167712a61a9ddfd5bd97350d7
Ricardo Souza <souzaariick@gmail.com>
2020-11-26 19:57:10 -0300

    new test

--- app/src/main/java/com/rsf/easycep/MainActivity.kt
+++ app/src/main/java/com/rsf/easycep/MainActivity.kt
@@ -2,9 +2,13 @@ package com.rsf.easycep
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rsf.kteasycep.EasyCep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        EasyCep()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
---
+++ kteasycep/src/main/java/com/rsf/kteasycep/EasyCep.kt
@@ -0,0 +1,7 @@
+package com.rsf.kteasycep
+
+class EasyCep {
+    fun retrieveCep(cep: String): String {
+        return cep;
+    }
+}
